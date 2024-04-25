73e2c5e12040c5f65108948a315b0bec9bf73b4f
satyamacn11 <satyam.arya@monitoredsecurity.com>
2024-04-26 00:26:38 +0530

    v2

--- aws_infra-tf/main.tf
+++ aws_infra-tf/main.tf
@@ -454,7 +454,7 @@ resource "aws_instance" "ec2_instance" {
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-#  user_data = file("script.sh")
+  user_data = file("script.sh")
 
   tags = {
     Name = "mss_dev_us-east-1_demo_arya-ec2"
